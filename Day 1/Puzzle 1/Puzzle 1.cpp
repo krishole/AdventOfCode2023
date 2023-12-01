43b8958d19a81fa83306bbca591ebd1422937599
Kris Hole <kris_hole@hotmail.com>
2023-12-01 14:55:22 +0000

    Day 1, Puzzle 1

---
+++ Day 1/Puzzle 1/Puzzle 1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+	std::ifstream file("calibration.txt"); // Replace "example.txt" with your file name
+
+	int total = 0;
+
+	if (file.is_open()) {
+		std::string line;
+		while (std::getline(file, line)) {
+			auto firstIterator = std::find_if(line.begin(), line.end(), [](char c) { return std::isdigit(c); });
+			auto lastIterator = std::find_if(line.rbegin(), line.rend(), [](char c) { return std::isdigit(c); });
+
+			if (firstIterator != line.end() && lastIterator != line.rend()) {
+				int first = *firstIterator - '0';
+				int last = *lastIterator - '0';
+
+				total += (first * 10) + last;
+			}
+		}
+		file.close();
+	} else {
+		std::cout << "Unable to open file" << std::endl;
+	}
+
+	std::cout << total << std::endl;
+
+	return 0;
+}
