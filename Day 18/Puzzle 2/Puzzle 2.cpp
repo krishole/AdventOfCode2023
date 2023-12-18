92d12e9a7fd05742df8d9dd3ae732263bb991c6e
Kris Hole <kris_hole@hotmail.com>
2023-12-18 14:52:21 +0000

    Day 18, Puzzle 1 & 2, Shoelace!

--- Day 18/Puzzle 2/Puzzle 2.cpp
+++ Day 18/Puzzle 2/Puzzle 2.cpp
@@ -1,62 +1,54 @@
 #include <iostream>
 #include <fstream>
-#include <queue>
 #include "Utils.h"
 
-std::vector<std::pair<int, int>> s_Directions = { { 0,-1}, {0, 1},{-1, 0}, {1,0} };
-
+typedef long long ll;
 struct SPoint {
-	int x, y;
+	ll x, y;
 };
+
+static std::vector<std::pair<ll, ll>> s_Directions = { { 1,0}, {0, 1},{-1, 0}, {0,-1} };
 static std::vector<SPoint> s_Polygon;
 static int s_TotalDistance = 0;
 
 void ProcessLine(const std::string& line) {
 	std::vector<std::string> chunks = ParseString(line, " ");
 
-	int dir = 0;
-	char d = chunks[0][0];
-	if (d == 'U') {
-		dir = 0;
-	} else if (d == 'D') {
-		dir = 1;
-	} else if (d == 'L') {
-		dir = 2;
-	} else if (d == 'R') {
-		dir = 3;
-	}
+	std::string hex = chunks[2].substr(2, 5);
+	std::string hex2 = chunks[2].substr(7, 1);
 
-	int steps = std::stoi(chunks[1]);
+	ll direction = std::stoi(hex2);
+	ll steps = std::stoll(hex, 0, 16);
+
 	const SPoint last = s_Polygon.back();
-	int x = last.x + (s_Directions[dir].first * steps);
-	int y = last.y + (s_Directions[dir].second * steps);
+	ll x = last.x + (s_Directions[direction].first * steps);
+	ll y = last.y + (s_Directions[direction].second * steps);
 
-	s_TotalDistance += steps;
 	s_Polygon.push_back({ x, y });
+	s_TotalDistance += steps;
 }
 
 void Dig() {
 	//shoelace
-	int area = 0;
+	ll area = 0;
 	size_t n = s_Polygon.size();
 	for (size_t i = 0; i < n; ++i) {
-		int x1 = s_Polygon[i].x;
-		int y1 = s_Polygon[i].y;
-		int x2 = s_Polygon[(i + 1) % n].x;
-		int y2 = s_Polygon[(i + 1) % n].y;
+		ll x1 = s_Polygon[i].x;
+		ll y1 = s_Polygon[i].y;
+		ll x2 = s_Polygon[(i + 1) % n].x;
+		ll y2 = s_Polygon[(i + 1) % n].y;
 
 		area += (x1 * y2 - x2 * y1);
 	}
 
 	area = std::abs(area) / 2;
-	int total = area + 1 + s_TotalDistance / 2;
+	ll total = area + 1 + s_TotalDistance / 2;
 	std::cout << "Area: " << total << std::endl;
 }
 
-
 int main() {
 	std::ifstream file("input.txt");
-	s_Polygon.push_back({ 0, 0 });
+	s_Polygon.push_back({ 0,0 });
 
 	if (file.is_open()) {
 		std::string line;
@@ -64,7 +56,8 @@ int main() {
 			ProcessLine(line);
 		}
 		file.close();
-	} else {
+	}
+	else {
 		std::cout << "Unable to open file" << std::endl;
 	}
 	Dig();
